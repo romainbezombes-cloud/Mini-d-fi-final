5c8ab78ccad9947e02f7c822087edf791dd411be
“romainbezombes-cloud” <romain.bezombes@mmibordeaux.com>
2026-01-11 13:00:44 +0100

    v3 (champs rgb non fonctionnels pour le moment)

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 
 //lien entre html et js
 const generateur = document.querySelector("#generateur");
-let titre = document.querySelector("h1");
+let couleur = document.querySelector("#couleur");
 const liste = document.querySelector("#liste");
+const generer = document.querySelector("#generer");
 
-//actions déclenchées par le click du bouton
+
+const rouge = document.querySelector("rouge");
+const vert = document.querySelector("vert");
+const bleu = document.querySelector("bleu");
+
+
+//actions déclenchées par le click du bouton "génerer une couleur aléatoire"
 generateur.addEventListener("click", function(){
 
     //génère un code hexadécimal aléatoire (j'ai trouvé la partie Math random sur internet)
-    titre.textContent = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+    couleur.textContent = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
 
     //changement de la couleur du texte en celle correspondant au code Hexa affiché
     //càd assigner le contenu du texte généré précedemment à la couleur de ce texte
-    titre.style.color = titre.textContent
+    couleur.style.color = couleur.textContent
 
     //creation d'un item de liste
     let li = document.createElement("li")
 
-    li.textContent = titre.textContent
+    //ajout de l'item (la couleur générée) dans la liste 
+    li.textContent = couleur.textContent
     liste.appendChild(li);
-    li.style.color = titre.textContent
+    //donne la couleur générée à l'item concerné (comme à la ligne 15)
+    li.style.color = couleur.textContent
 });
 
+//actions déclenchées par le click du bouton "génerer"
+generer.addEventListener("click", function(){
+    //actions des champs couleurs
+    if(rouge.value === "" ) {
+        alert("test 2")
+    }
+});
